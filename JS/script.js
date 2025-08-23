49408de62ed0603d8c2526a4eeafb59f51e29bff
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-08-23 23:44:42 +0600

    payooo method added and more element added

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+// login button functionality 
+
+document.getElementById('btn-login')
+    .addEventListener('click', function (){
+        const mobileNumber = 88017123456789 ;
+        const pinNumber = 1234;
+
+
+        const mobileNumberValue = document.getElementById('mobile-number').value
+        const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+        const pinNumberValue = document.getElementById('pin-number').value;
+        const pinNumberValueConverted = parseInt(pinNumberValue);
+
+        
+        // console.log(typeof mobileNumberValueConverted,typeof pinNumberValueConverted);
+        
+        if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber ){
+            window.location.href = '../html/home.html'
+            
+        }else{
+           alert('Invalid Credential')
+            
+        }
+    })
