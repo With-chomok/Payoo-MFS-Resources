2fe24a3f28a110e6dfc85ff2dd3b4a7c04b4d5da
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-08-24 13:31:38 +0600

    add and cashput button troggle added

--- JS/home.js
+++ JS/home.js
@@ -15,7 +15,35 @@ document.getElementById('add-money-btn')
         document.getElementById('available-balance').innerText = totalBalance
     })
 
+// trogolling features 
+
+document.getElementById('add-button')
+    .addEventListener('click', function(){
+        document.getElementById('cash-out-parent').style.display = 'none';
+        document.getElementById('transfer-money-parent').style.display = 'none';
+        document.getElementById('get-bonus-parent').style.display = 'none';
+        document.getElementById('pay-bill-parent').style.display = 'none';
+        
+        document.getElementById('add-money-parent').style.display = 'block'
+
+ })
+        // document.getElementById('transfer-money-parent').style.display = 'block';
+        // document.getElementById('get-bonus-parent').style.display = 'block';
+        // document.getElementById('pay-bill-parent').style.display = 'block';
+   
+
+document.getElementById('cashout-button')
+    .addEventListener('click', function(){
+        document.getElementById('add-money-parent').style.display = 'none';
+        document.getElementById('transfer-money-parent').style.display = 'none';
+        document.getElementById('get-bonus-parent').style.display = 'none';
+        document.getElementById('pay-bill-parent').style.display = 'none';
+        
+        document.getElementById('cash-out-parent').style.display = 'block'
+
+ })
 
 
 
-
+ 
+     
